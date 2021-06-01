3e9122ffb200cd1cfe56fe5aa45bd2b6c93944f5
Andre Wuepping <Andre.Wuepping@crautomation.nz>
2021-06-02 06:25:38 +1200

    Made changes to Robot definition

--- arduino/opencr_arduino/opencr/libraries/turtlebot3/examples/turtlebot3_burger/turtlebot3_core/turtlebot3_burger.h
+++ arduino/opencr_arduino/opencr/libraries/turtlebot3/examples/turtlebot3_burger/turtlebot3_core/turtlebot3_burger.h
@@ -21,14 +21,14 @@
 
 #define NAME                             "Burger"
 
-#define WHEEL_RADIUS                     0.033           // meter
-#define WHEEL_SEPARATION                 0.160           // meter (BURGER : 0.160, WAFFLE : 0.287)
-#define TURNING_RADIUS                   0.080           // meter (BURGER : 0.080, WAFFLE : 0.1435)
-#define ROBOT_RADIUS                     0.105           // meter (BURGER : 0.105, WAFFLE : 0.220)
-#define ENCODER_MIN                      -2147483648     // raw
-#define ENCODER_MAX                      2147483648      // raw
+#define WHEEL_RADIUS                     0.0356          // meter                                       // 35.6mm
+#define WHEEL_SEPARATION                 0.172967        // meter (BURGER : 0.160, WAFFLE : 0.287)      // 172.967mm
+#define TURNING_RADIUS                   0.086483        // meter (BURGER : 0.080, WAFFLE : 0.1435)     // 86.4835
+#define ROBOT_RADIUS                     0.105           // meter (BURGER : 0.105, WAFFLE : 0.220)      //
+#define ENCODER_MIN                      -2147483648     // raw                                         //
+#define ENCODER_MAX                      2147483648      // raw                                         //
 
-#define MAX_LINEAR_VELOCITY              (WHEEL_RADIUS * 2 * 3.14159265359 * 61 / 60) // m/s  (BURGER : 61[rpm], WAFFLE : 77[rpm])
+#define MAX_LINEAR_VELOCITY              (WHEEL_RADIUS * 2 * 3.14159265359 * 46 / 60) // m/s  (BURGER: 61[rpm], WAFFLE: 77[rpm], XM430-W350-T: 46[rpm])       //
 #define MAX_ANGULAR_VELOCITY             (MAX_LINEAR_VELOCITY / TURNING_RADIUS)       // rad/s
 
 #define MIN_LINEAR_VELOCITY              -MAX_LINEAR_VELOCITY  
