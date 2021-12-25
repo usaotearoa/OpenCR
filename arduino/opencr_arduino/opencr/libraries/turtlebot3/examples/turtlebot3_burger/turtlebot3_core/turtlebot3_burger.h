8a5247d48a93d483a312db1aee187011b558800b
usaotearoa <usaotearoa2020@gmail.com>
2021-12-25 19:57:56 +1300

    intial comit before source skinning

--- arduino/opencr_arduino/opencr/libraries/turtlebot3/examples/turtlebot3_burger/turtlebot3_core/turtlebot3_burger.h
+++ arduino/opencr_arduino/opencr/libraries/turtlebot3/examples/turtlebot3_burger/turtlebot3_core/turtlebot3_burger.h
@@ -1,19 +1,3 @@
-/*******************************************************************************
-* Copyright 2016 ROBOTIS CO., LTD.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
-
 /* Authors: Just me. */
 
 #ifndef TURTLEBOT3_BURGER_H_
@@ -21,10 +5,10 @@
 
 #define NAME                             "Burger"
 
-#define WHEEL_RADIUS                     0.0356          // meter                                       // 35.6mm
-#define WHEEL_SEPARATION                 0.172967        // meter (BURGER : 0.160, WAFFLE : 0.287)      // 172.967mm
-#define TURNING_RADIUS                   0.086483        // meter (BURGER : 0.080, WAFFLE : 0.1435)     // 86.4835
-#define ROBOT_RADIUS                     0.245           // meter (BURGER : 0.105, WAFFLE : 0.220)      //
+#define WHEEL_RADIUS                     0.036           // meter                                       // 36mm
+#define WHEEL_SEPARATION                 0.172500        // meter (BURGER : 0.160, WAFFLE : 0.287)      // 172.500mm
+#define TURNING_RADIUS                   0.086250        // meter (BURGER : 0.080, WAFFLE : 0.1435)     // 86.25mm
+#define ROBOT_RADIUS                     0.12825         // meter (BURGER : 0.105, WAFFLE : 0.220)      //
 #define ENCODER_MIN                      -2147483648     // raw                                         //
 #define ENCODER_MAX                      2147483648      // raw                                         //
 
